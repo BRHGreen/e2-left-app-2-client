91e608e04017452bd67657388922f6dcc762e3b2
BRHGreen <benedictgreen@gmail.com>
2018-01-16 20:29:16 +0000

    getting user data into the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { getUser } from '../graphql/user'
+import { graphql, compose } from 'react-apollo'
+
 
 const Navbar = ({
-    // getUser
+    user
 }) => (
 <nav>
+    {user.getUser &&
     <ul>
         <li><Link to={'/'}>E2 Left</Link></li>
-        {/* <li><Link to={'/'}>{getUser.username}</Link></li> */}
+        <li><Link to={'/user-profile'}>{user.getUser.username}</Link></li>
         <li>Logout</li>
     </ul>
+    }   
 </nav>
 )
 
-export default Navbar;
+const NavbarWithMutations = compose(
+    graphql(getUser, { name: "user" })
+)(Navbar)
+
+export default NavbarWithMutations;
