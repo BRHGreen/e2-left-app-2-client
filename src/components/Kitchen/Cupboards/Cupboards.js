9247563d657d6942732f805f5202af0fe7e428a9
BRHGreen <benedictgreen@gmail.com>
2018-06-25 23:11:13 +0100

    looping cupboards

--- src/components/Kitchen/Cupboards/Cupboards.js
+++ src/components/Kitchen/Cupboards/Cupboards.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CupboardsMainlandWest from './CupboardsMainlandWest'
 import CupboardsMainlandNorth from './CupboardsMainlandNorth'
 import CupboardsPeninsula from './CupboardsPeninsula'
 import CupboardsIsland from './CupboardsIsland'
 
-const Cupboards = () => {
-  return (
-    <div className="kitchen__cupboards">
-      <CupboardsMainlandWest />
-      <CupboardsMainlandNorth />
-      <CupboardsPeninsula />
-      <CupboardsIsland />
-    </div>
-  )
+class Cupboards extends React.Component {
+  state = {
+
+  }
+  handleViewChange (e) {
+    console.log('e', e)
+  }
+  
+  render () {
+    const cupboards = [
+      'mainland-west',
+      'mainland-north',
+      'peninsula',
+      'island',
+    ]
+    return (
+      <div className="kitchen__cupboards">
+      {
+        cupboards.map((cupboard, i)=>(
+          <div 
+            className={`kitchen__cupboards__${cupboard}`}
+            handleViewChange={(e) => this.handleViewChange (e)}
+          >
+            {`kitchen__cupboards__${cupboard}`}
+          </div>
+        ))
+      }
+      </div>
+    )
+  }
 }
 
 export default Cupboards
