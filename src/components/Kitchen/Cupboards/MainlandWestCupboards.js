12e73429c2f24f5a93d11f440e829ed900e2786a
BRHGreen <benedictgreen@gmail.com>
2018-07-17 23:03:43 +0100

    mainland west basic layout

--- src/components/Kitchen/Cupboards/MainlandWestCupboards.js
+++ src/components/Kitchen/Cupboards/MainlandWestCupboards.js
@@ -8,23 +8,64 @@ const MainlandWestCupboards = (
 ) => {
   console.log('MainlandWestCupboards', props.mainlandWestCupboards
 )
-  return (
+  const cupboardClassName = ({ landMass, cupboardNumber }, position) => (
+    camelCaseToDash(
+      `
+       ${landMass}__cupboard
+       ${landMass}__cupboard--${position}
+       ${landMass}--${cupboardNumber}
+       `
+    )
+  )
 
+  return (
     <div className="mainland-west-cupboards__container">MainlandWestCupboards
     {
       props.mainlandWestCupboards.loading 
       ? <div>Loading</div>
-      : <div>{props.mainlandWestCupboards.getMainlandWestCupboards.map((cupboard, i) => {
-          const cupboardClassName = camelCaseToDash(`${cupboard.landMass}--${cupboard.cupboardNumber}`)
-          return (
-            <div key={i} className={cupboardClassName}>
-              {}
-            </div>
+      : <div>
+          <div>
+            {props.mainlandWestCupboards.getMainlandWestCupboards.map((cupboard, i) => {
+              if (cupboard.cupboardNumber <= 3) {
+                return (
+                  <div key={i} className={`${cupboardClassName(cupboard, "top")}`}>
+                    {cupboardClassName(cupboard, "top")}
+                  </div>
+                )
+              }
+            }
           )
         }
-      )
-    }
+      </div>
+        <div className="mainland-west__cupboard">sink blank</div>
+        <div>
+          {props.mainlandWestCupboards.getMainlandWestCupboards.map((cupboard, i) => {
+            if (cupboard.cupboardNumber > 3 && cupboard.cupboardNumber <= 5) {
+              return (
+                <div key={i} className={`${cupboardClassName(cupboard, "top")}`}>
+                  {cupboardClassName(cupboard, "top")}
+                </div>
+              )
+            }
+          }
+        )
+      }
     </div>
+        <div>bottom break</div>
+        <div>
+          {props.mainlandWestCupboards.getMainlandWestCupboards.map((cupboard, i) => {
+            if (cupboard.cupboardNumber > 5 && cupboard.cupboardNumber <= 11) {
+              return (
+                <div key={i} className={`${cupboardClassName(cupboard, "bottom")}`}>
+                  {cupboardClassName(cupboard, "bottom")}
+                </div>
+              )
+            }
+          }
+        )
+      }
+    </div>
+      </div>
     }
     </div>
   )
