607e3fceee43cad3358c94959ab963f98edeff1b
BRHGreen <benedictgreen@gmail.com>
2018-08-14 18:18:40 +0100

    dropdown finally in?

--- src/components/common/Dropdown.js
+++ src/components/common/Dropdown.js
@@ -1,45 +1,40 @@
 import React from 'react';
+
 const Dropdown = (props) => {
-  const {
-    isOpen,
-    onClose,
-    header,
-    menuItems,
-    onChangeHandler,
-    displayValue,
-  } = props
+    const {
+      isOpen,
+      onClose,
+      header,
+      menuItems,
+      onChangeHandler,
+      displayValue
+    } = props
+    return (
+      <details className="accordion" open={isOpen}>
+        <summary className="accordion-header">
+          {header}
+        </summary>
 
-  return (
-    <details className="accordion" open={isOpen}>
-      <summary className="accordion-header">
-        {header}
-      </summary>
-      {console.log(this.props)}
-      <div className="accordion-body">
-        <ul className="menu menu-nav">
-          {
-            menuItems && menuItems.map((item, i) => {
-              { console.log('item', item) }
-              if (item) {
-                return (
-                  <li
-                    className="menu-item"
-                    key={i}
-                  >
-                    <a onClick={() => onChangeHandler(item)}>{displayValue ? item[displayValue] : item}</a>
-                  </li>
-                )
+        <div className="accordion-body">
+          <ul className="menu menu-nav">
+            {
+              menuItems && menuItems
+                .map((item, i) => {
+                  if(item) {
+                    return (
+                      <li className="menu-item" key={i}>
+                        <a onClick={() => onChangeHandler(item)}>{displayValue ? item[displayValue] : item}</a>
+                      </li>
+                    )
+                  }
+                  return null
+                })
               }
-              return null
-            }
-            )
-          }
-          <li onClick={onClose}>close</li>
-        </ul>
-      </div>
-    </details>
-  )
-
+            <li onClick={onClose}>close</li>
+          </ul>
+        </div>
+      </details>
+    )
 }
 
 export default Dropdown
