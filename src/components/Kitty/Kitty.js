c8a60ed21512e8127a73087eea60726334539890
BRHGreen <benedictgreen@gmail.com>
2018-08-30 21:18:46 +0100

    writing to db from UI

--- src/components/Kitty/Kitty.js
+++ src/components/Kitty/Kitty.js
@@ -1,23 +1,50 @@
 import React from 'react';
+import { compose, graphql } from 'react-apollo';
+import { createKittyStatement } from '../../graphql/kitty';
 
 class Kitty extends React.Component {
+  state = {
+    csvContent: ''
+  }
 
-  handleSubmit (e, file) {
-    e.preventDefault()
-    console.log('e', e)
-    console.log('file', file)
+  parseCsv = async () => {
+    // console.log('this.state', this.state.csvContent)
+    const arr = [this.state.csvContent]
+    // console.log('arr', arr)
+    console.log('split',this.state.csvContent.split(",", 6))
+    const fields = this.state.csvContent.split(",", 6)
+    console.log('fields', fields)
+    await this.props.createKittyStatement({
+      variables: {
+        date: fields[0],
+        counterParty: fields[1],
+        reference: fields[2],
+        type: fields[3],
+        amount: fields[4],
+        balance: fields[5],
+      }
+    })
+  }
+
+  onChangeHandler (e) {
+    this.setState({ csvContent: e.target.value })
+    console.log('this.state', this.state.csvContent)
   }
   
   render() {
+    console.log(this.props)
     return (
-    <div>
-      <form onSubmit={(e) => this.handleSubmit(e)}>
-        <input type="file" />
-        <input type="submit"/>
-      </form>
-    </div>
-  )
+      <div className='page__container'>
+        <textarea style={{height: '500px', width: '500px'}} onChange={(e) => this.onChangeHandler(e)}/>
+        <br />
+        <button onClick={() => this.parseCsv()}>Parse</button>
+      </div>
+    )
   }
 }
 
-export default Kitty;
+export default compose(
+  graphql(createKittyStatement,
+    {name: 'createKittyStatement'}
+  )
+)(Kitty)
---
+++ src/graphql/kitty.js
@@ -0,0 +1,29 @@
+import gql from 'graphql-tag'
+
+export const createKittyStatement = gql`
+mutation(
+    $date: String,
+    $counterParty: String,
+    $reference: String,
+    $type: String,
+    $amount: Float,
+    $balance: Float,
+    $openingBalance: Float,
+  ) {
+    createKittyStatement(
+    date: $date
+    counterParty: $counterParty
+    reference: $reference
+    type: $type
+    amount: $amount
+    balance: $balance
+    openingBalance: $openingBalance
+    ) {
+      ok
+      errors {
+        path
+        message
+      }
+    }
+  }
+`
