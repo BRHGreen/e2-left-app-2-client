bc1a679b2dcf7beb46567c9a86af918ae19d47cd
BRHGreen <benedictgreen@gmail.com>
2018-08-31 23:27:55 +0100

    table

--- src/components/Kitty/Kitty.js
+++ src/components/Kitty/Kitty.js
@@ -2,11 +2,28 @@ import React from 'react';
 import { compose, graphql } from 'react-apollo';
 import { getAllKittyStatements } from '../../graphql/kitty';
 
-const Kitty = (props, { getAllKittyStatements }) => {
+const Kitty = (props, { getAllKittyStatements: getAllKittyStatements }) => {
   console.log('getAllKittyStatements', getAllKittyStatements)
   console.log('props', props)
   return (
-    <div>Kitty</div>
+    <div className="page__container">
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th>name</th>
+            <th>genre</th>
+            <th>release date</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="active">
+            <td>The Shawshank Redemption</td>
+            <td>Crime, Drama</td>
+            <td>14 October 1994</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
 
